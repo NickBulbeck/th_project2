619a41cce080d0faed92d74e82f5287e475612f9
NickBulbeck <bulbecknick@gmail.com>
2019-10-09 16:35:56 +0100

    Refined the easter egg.

--- js/frogstar.js
+++ js/frogstar.js
@@ -8,7 +8,7 @@
 const vowels = ['a','e','i','o','u','ai','ee','ea','oo','ou','oa','oi'];
 const openers = ['B','C','D','F','G','H','J','K','L','M','N','P','R','S','T','Qu',
                   'V','W','Y','Z','Br','Bl','Cr','Cl','Dr','Fr','Fl','Gl','Gr','Pr','Pl','St','Str',
-                  'Sl','Tr','Th','Vr','Zr'];
+                  'Sl','Tr','Th','Vr'];
 const closers = ['b','ck','d','ff','g','j','l','m','rm','n','rn','p','lp','rp','r',
                   's','ts','rs','t','rt','ls','lt','rv','rx','rz','z','sh'
                   ,'dle','ble','ffle','x'];
@@ -27,6 +27,8 @@ aCloser = () => {
 
 // This function returns a random name. The reason for the for-loop is to increase the likelihood of a syllable ending
 // in -ble, -dle or -fle (ie, length > 2 characters). I think we can all agree that those names are funnier.
+// They're also easier to pronounce if you don't have a vowel pair followed by a consonant pair (e.g. 'ealv'); 
+// hence lintilla and allitnil.
 beeblebrox = () => {
   let quordle = "";
   for (let i = 0; i < 10; i++) {
@@ -35,15 +37,22 @@ beeblebrox = () => {
       break;
     }
   }
-  return anOpener() + aVowel() + aCloser() + " " +
-         anOpener() + aVowel() + quordle + anOpener().toLowerCase() + aVowel() + aCloser();
+  let lintilla = "";
+  let allitnil = "";
+  do {
+    lintilla = aVowel() + aCloser();
+  } while (lintilla.length > 3);
+  do {
+    allitnil = aVowel() + aCloser();
+  } while (allitnil.length > 3);
+  return anOpener() + lintilla + " " +
+         anOpener() + aVowel() + quordle + anOpener().toLowerCase() + allitnil;
 }
 
 // This function takes in a name and converts it into an email address of the form forname.surname@... etc.
 // The significance of the email address will be grasped by afficionados of the Hitch-Hiker Trilogy!
 dolmansaxlil = name => {
-  name = name.toLowerCase();
-  let gargravarr = name.replace(" ",".");
+  let gargravarr = name.toLowerCase().replace(" ",".");
   gargravarr += "@dolmansaxlil.frogstar";
   return gargravarr;
 }
@@ -51,8 +60,6 @@ dolmansaxlil = name => {
 let name = beeblebrox();
 let email = dolmansaxlil(name);
 
-console.log (name + "; " + email);
-console.log (vowels.length * closers.length * openers.length);
 
 /* ToDo: Create a function that does the following:
     1) Gets every div with a class of student details
